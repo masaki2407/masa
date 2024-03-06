93e75d738eac86d924e15bfd53927df812388bd6
masaki2407 <masakihayase2407>
2024-03-06 15:47:51 +0900

    適切なコミットコメント2

--- routes/web.php
+++ routes/web.php
@@ -16,4 +16,4 @@ use App\Http\Controllers\PostController;
 
 Route::get('/', function () { return view('welcome'); });
 
-Route::get('/posts',[PostController::class, 'index']);
+Route::get('/posts', [PostController::class, 'index']);
